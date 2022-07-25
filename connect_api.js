eb5700bd59df549a5b88ea88ddaf8b44c10e943e
chinedugabriel <chinedugabriel29@gmail.com>
2022-07-26 00:22:46 +0100

    created a getdata funtion that would fetch and a .the to covert the response into an array

---
+++ connect_api.js
@@ -0,0 +1,19 @@
+const getData = async () => {
+    let request = await fetch('test.csv');
+    if(request.status === 200){
+        return request.text();
+    }throw"File not found";
+};
+
+getData().then(response => {
+    // console.log(response)
+    let table = response.split('\n').slice(1);
+    console.log(table)
+     table.forEach(row=>{
+        let columns = row.split(',')
+        let year = columns[0];
+        let temp =columns[1];
+        
+        console.log(year, temp)
+    })
+})
