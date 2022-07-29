6831bb54d0b38a8698b7aa686b33f43ba56d6c3a
chinedugabriel <chinedugabriel29@gmail.com>
2022-07-29 03:09:40 +0100

    modified the csv file and connected the the chart to the getData function responsible for generating the data

--- connect_api.js
+++ connect_api.js
@@ -1,20 +1,57 @@
 const getData = async () => {
-    let request = await fetch('test.csv');
-    if(request.status === 200){
-        return request.text();
-    }throw"File not found";
+    let request = await fetch('ZonAnn.Ts+dSST.csv');
+
+        let response = await request.text();
+        let table = response.split('\n').slice(1);
+
+        table.forEach(row=>{
+
+            let columns = row.split(',')
+            let year = columns[0];
+            xlabels.push(year);
+    
+            let temp =columns[1];
+            yTemps.push(temp);
+    
+            console.log(year, temp)
+        })
+
+    
 };
 
-getData().then(response => {
-    // console.log(response)
-    let table = response.split('\n').slice(1);
-    console.log(table)
-     table.forEach(row=>{
-        let columns = row.split(',')
-        let year = columns[0];
-        let temp =columns[1];
-        
-        console.log(year, temp)
-    })
-}).catch(error => console.log(error))
+// getData().then(response => {
+//     // console.log(response)
+//     // console.log(table)
+//      table.forEach(row=>{
 
+//         let columns = row.split(',')
+//         let year = columns[0];
+//         xlabels.push(year);
+
+//         let temp =columns[1];
+//         yTemps.push(temp);
+
+//         console.log(year, temp)
+//     })
+// }).catch(error => console.log(error))
+
+
+
+// let getData = async ()=> {
+//     let request = await fetch("test.csv");
+//     if(request.status === 200){
+//         return await request.text();
+//     }throw"File not found check your code"
+    
+// };
+// getData().then(response =>{
+//     let table = response.split("\n").slice(1);
+//     table.forEach(elt=>{
+//         let row = elt.split(",");
+//         let year = row[0];
+//         let temp = row[1]
+//         console.log(year,temp);
+//     })
+
+    
+// }).catch(error=>console.log(error))
